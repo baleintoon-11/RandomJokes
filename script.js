1fd470aec61dda2d5a36ded062291e9d15ccc085
Prashanth <prashanth@toonlads.com>
2025-03-29 16:14:47 -0500

    12

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 document.getElementById("getJoke").addEventListener("click", getJoke);
 
 async function getJoke() {
-    const loader = document.getElementById("loader");
     const jokeText = document.getElementById("jokeText");
 
-    loader.classList.remove("hidden");
     jokeText.classList.remove("visible"); // fade out text
 
     try {
         const res = await fetch("https://official-joke-api.appspot.com/random_joke");
         const data = await res.json();
 
-        // Only set new text after short delay
         setTimeout(() => {
             jokeText.textContent = `${data.setup} 😂 ${data.punchline}`;
             jokeText.classList.add("visible");
@@ -19,8 +16,5 @@ async function getJoke() {
     } catch (err) {
         jokeText.textContent = "❌ Couldn't fetch joke.";
         jokeText.classList.add("visible");
-    } finally {
-        loader.classList.add("hidden");
     }
 }
-
